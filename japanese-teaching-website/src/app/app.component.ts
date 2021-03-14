a267368b90d110a567b93dc300b6aedce137dc79
ThompsonNJ <nicholasjohnthompson@gmail.com>
2021-03-13 22:53:27 -0500

    added font awesome social media icons

--- japanese-teaching-website/src/app/app.component.ts
+++ japanese-teaching-website/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { faFacebookF, faGithub, faGoogle, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'japanese-teaching-website';
+  faFacebook = faFacebookF;
+  faTwitter = faTwitter;
+  faGoogle = faGoogle;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedinIn;
+  faGithub = faGithub;
 }
--- japanese-teaching-website/src/app/app.module.ts
+++ japanese-teaching-website/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { SecondComponent } from './second/second.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
